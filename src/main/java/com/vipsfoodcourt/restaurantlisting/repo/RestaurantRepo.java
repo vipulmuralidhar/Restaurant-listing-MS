49cb1a5f8cefbea8191bb965758ee8e629abf844
vipulm <vipulmuralidhar@gmail.com>
2024-10-23 03:04:05 -0500

    adding jenkins file

--- src/main/java/com/vipsfoodcourt/restaurantlisting/repo/RestaurantRepo.java
+++ src/main/java/com/vipsfoodcourt/restaurantlisting/repo/RestaurantRepo.java
@@ -6,4 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RestaurantRepo extends JpaRepository<Restaurant,Integer> {
 
 
+   /*
+      If we want to call complex queries using jpa repository,quries would be like this
+   @Repository
+    public interface RestaurantRepo extends JpaRepository<Restaurant, Long> {
+        @Query("SELECT r FROM Restaurant r WHERE r.rating > :rating")
+        List<Restaurant> findRestaurantsWithHighRatings(@Param("rating") double rating);
+    }
+*/
+
+
 }
